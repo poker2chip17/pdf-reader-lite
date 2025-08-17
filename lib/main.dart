50c3f7ae8a64ab6ddaafd7ff1f120ad7af894910
poker2chip17 <68142168+poker2chip17@users.noreply.github.com>
2025-08-17 22:41:59 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+```yaml
+name: pdf_reader_lite
+publish_to: 'none'
+
+environment:
+  sdk: ">=3.3.0 <4.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+  file_picker: ^8.0.0
+  shared_preferences: ^2.2.2
+  pdfx: ^2.6.0
+
+flutter:
+  uses-material-design: true
+```
